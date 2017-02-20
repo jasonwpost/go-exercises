1ad76dc02fa81c682f907e6d8ff30279d39a2c9a
Jason Post <jason.w.post@gmail.com>
2017-02-20 17:14:46 +1300

    logic, for loops

---
+++ 3.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+  i := 1
+
+  for i <= 10 {
+    fmt.Println(i)
+    i++
+  }
+
+  // Relational Operators include ==, !=, <, >, <=, and >=
+
+	// You can also define a for loop like this, but you need semicolons
+
+	for j := 0; j < 5; j++ {
+
+		fmt.Println(j);
+
+	}
+}
