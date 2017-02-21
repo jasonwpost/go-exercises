aa575ece28b1f114346ed502615be9b92dff102e
Jason Post <jason.w.post@gmail.com>
2017-02-21 17:16:12 +1300

    interfaces

---
+++ 10.go
@@ -0,0 +1,41 @@
+// interfaces
+
+package main
+
+import "fmt"
+import "math"
+
+func main() {
+
+  rect := Rectangle{20, 50}
+  circ := Circle{4}
+
+  fmt.Println("Rectangle Area =", getArea(rect))
+  fmt.Println("Circle Area =", getArea(circ))
+
+}
+
+type Shape interface {
+  area() float64
+}
+
+type Rectangle struct {
+  height float64
+  width float64
+}
+
+type Circle struct {
+  radius float64
+}
+
+func (r Rectangle) area() float64 {
+  return r.height * r.width
+}
+
+func (c Circle) area() float64 {
+  return math.Pi * math.Pow(c.radius, 2)
+}
+
+func getArea(shape Shape) float64 {
+  return shape.area()
+}
