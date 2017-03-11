401ad87403fadb3e05794bc3ac93ba4976a7b1b4
Jason Post <jason.w.post@gmail.com>
2017-03-11 08:41:52 +0100

    listen and serve

---
+++ Todd-McLeod-Go-Web-Dev-Tutorial/03-net-http-package/01-listen-and-serve/main.go
@@ -0,0 +1,22 @@
+// Handler :
+// type Handler interface {
+//  ServeHTTP(ResponseWriter, *Request)
+//}
+
+package main
+
+import (
+  "fmt"
+  "net/http"
+)
+
+type hotdog int
+
+func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request){
+  fmt.Fprintln(w, "Any code you want in this func")
+}
+
+func main(){
+  var d hotdog
+  http.ListenAndServe(":8080", d)
+}
