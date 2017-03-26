62b09996452552b9239960c1baaa698978933c90
Jason Post <jason.w.post@gmail.com>
2017-03-26 12:24:21 +0200

    create photo uploader and hash

--- Todd-McLeod-Go-Web-Dev-Tutorial/10-photo-blog/main.go
+++ Todd-McLeod-Go-Web-Dev-Tutorial/10-photo-blog/main.go
@@ -5,6 +5,11 @@ import (
   "net/http"
   "github.com/satori/go.uuid"
   "strings"
+  "fmt"
+  "io"
+  "crypto/sha1"
+  "os"
+  "path/filepath"
 )
 
 var tpl *template.Template
@@ -21,7 +26,34 @@ func main(){
 
 func index(w http.ResponseWriter, req *http.Request){
   c := getCookie(w, req)
-  c = assignValues(w, c);
+  if req.Method == http.MethodPost {
+    mf, fh, err := req.FormFile("nf")
+    if err != nil {
+      fmt.Println(err)
+    }
+    defer mf.Close()
+    // create sha1 for file Name
+    ext := strings.Split(fh.Filename, ".")[1]
+    h := sha1.New()
+    io.Copy(h, mf)
+    fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
+    // create new file
+    wd, err := os.Getwd() // get current working dir
+    if err != nil {
+      fmt.Println(err)
+    }
+    path := filepath.Join(wd, "public", "pics", fname)
+    nf, err := os.Create(path)
+    if err != nil {
+      fmt.Println(err)
+    }
+    defer nf.Close()
+    // copy
+    mf.Seek(0, 0)
+    io.Copy(nf, mf) // copy into new file nf from mf
+    // add file names to this user
+    c = assignValues(w, c, fname);
+  }
   cookieSlice := strings.Split(c.Value, "|")
   tpl.ExecuteTemplate(w, "index.gohtml", cookieSlice)
 }
@@ -39,19 +71,10 @@ func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
   return c;
 }
 
-func assignValues(w http.ResponseWriter, c *http.Cookie) *http.Cookie {
-  p1 := "cookies.jpg"
-  p2 := "biscuits.jpg"
-  p3 := "jaffacakes.jpg"
+func assignValues(w http.ResponseWriter, c *http.Cookie, fname string) *http.Cookie {
   str := c.Value
-  if !strings.Contains(str, p1){
-    str += "|" + p1
-  }
-  if !strings.Contains(str, p2){
-    str += "|" + p2
-  }
-  if !strings.Contains(str, p3){
-    str += "|" + p3
+  if !strings.Contains(str, fname){
+    str += "|" + fname
   }
   c.Value = str
   http.SetCookie(w, c)
