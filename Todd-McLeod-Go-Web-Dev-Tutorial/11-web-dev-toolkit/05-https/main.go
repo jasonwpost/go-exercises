9401bf91a62aaead7e43dbc86e58362d6a327df1
Jason Post <jason.w.post@gmail.com>
2017-03-27 13:51:17 +0200

    https

---
+++ Todd-McLeod-Go-Web-Dev-Tutorial/11-web-dev-toolkit/05-https/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.ListenAndServeTLS(":10443", "cert.pem", "key.pem", nil)
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("This is an example server.\n"))
+}
+
+// Go to https://localhost:10443/ or https://127.0.0.1:10443/
+// list of TCP ports:
+// https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
+
+// Generate unsigned certificate
+// go run $(go env GOROOT)/src/crypto/tls/generate_cert.go --host=somedomainname.com
+// for example
+// go run $(go env GOROOT)/src/crypto/tls/generate_cert.go --host=localhost
