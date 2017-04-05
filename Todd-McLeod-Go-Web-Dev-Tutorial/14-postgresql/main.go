ede6b30c7224e340b8aaad76c30566c20cff0e94
Jason Post <jason.w.post@gmail.com>
2017-04-05 10:43:50 +0200

    basic connection to postgre

---
+++ Todd-McLeod-Go-Web-Dev-Tutorial/14-postgresql/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "fmt"
+  "database/sql"
+  _ "github.com/lib/pq"
+)
+
+func main(){
+  db, err := sql.Open("postgres", "postgres://bond:password@localhost/bookstore?sslmode=disable")
+  if err != nil {
+    panic(err)
+  }
+  defer db.Close()
+
+  err = db.Ping()
+  if err != nil {
+    panic(err)
+  }
+  fmt.Println("You connected to the database")
+}
